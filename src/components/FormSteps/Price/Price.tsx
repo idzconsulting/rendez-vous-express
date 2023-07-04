e812e277135ca353492d08143a5ca64ad4f956ff
yaac55 <yaacovc@balink.net>
2023-07-04 23:02:00 +0300

    add share

--- src/components/FormSteps/Price/Price.tsx
+++ src/components/FormSteps/Price/Price.tsx
@@ -5,9 +5,11 @@ import { useEffect, useState } from 'react';
 import styles from './Price.module.less';
 import { Engagement } from '../../../types/Engagement';
 import clsx from 'clsx';
-import { labelsMap } from '../../../types/Labels';
 import { IOnSelection } from '../../../types/IOnSelection';
 import { DiagnoscticsFetcher } from '../../../fetchers/role-fetchers/DiagnosticsFetcher';
+import WhatsApp from '../../Whatsapp/Share';
+import Share from '../../Whatsapp/Share';
+
 
 interface IPrice extends IOnSelection {
 }
@@ -15,6 +17,7 @@ interface IPrice extends IOnSelection {
 const Price = ({ onSelection }: IPrice) => {
     const [engagement, setEngagement] = useState<Engagement>();
     const [price, setPrice] = useState<any>()
+    const [htmlToShare, setHtmlToShare] = useState('');
 
     const getPrice = async () => {
         const { data } = await DiagnoscticsFetcher.price(currentEngagement.getCurrentMission());
@@ -34,72 +37,87 @@ const Price = ({ onSelection }: IPrice) => {
     }, []);
 
     return (
-        <StepCard title='Récapitulatif'>
-            <h3>Price: {price}€</h3>
+        <div>
+            <StepCard title='Récapitulatif'>
+                <div ref={(ref) => ref && setHtmlToShare(ref.innerHTML)} >
+                    <div className={styles.title}>Price: {price}€</div>
+                    
+                    <Divider />
 
-            <Divider />
-
-            <div className={styles.resume}>
-                <div className={styles.row}>
-                    <span className={styles.title}>Project</span>
-                    <span>{engagement?.project?.nom}</span>
-                </div>
-
-                <div className={styles.row}>
-                    <span className={styles.title}>Bien</span>
-                    <span>{engagement?.bien?.nom}</span>
-                </div>
-
-                <div className={styles.row}>
-                    <span className={styles.title}>Année de construction</span>
-                    <span>{engagement?.buildingYear?.nom}</span>
-                </div>
-
-                <div className={styles.row}>
-                    <span className={styles.title}>Superficie du bien</span>
-                    <span>{engagement?.surface?.nom}</span>
-                </div>
-
-                <div className={styles.row}>
-                    <span className={styles.title}>Annexes</span>
-                    <span>{engagement?.annexes}</span>
-                </div>
-
-                <div className={styles.row}>
-                    <span className={styles.title}>Diagnostics</span>
-                    <div>{engagement?.diagnostics?.map((diag) =>
-                        <span key={diag?.id}>{diag?.name}, </span>)}</div>
-                </div>
-
-                <div className={styles.row}>
-                    <span className={styles.title}>Informations</span>
-
-                    <div className={clsx(styles.infos, styles.row)}>
+                    <div className={styles.resume}>
                         <div className={styles.row}>
-                            <span className={styles.title}>Nom et prénom</span>
-                            <span>{engagement?.infos?.proprietaire_nom}</span>
+                            <div className={styles.title}>Projet</div>
+                            <div>{engagement?.project?.nom}</div>
                         </div>
 
                         <div className={styles.row}>
-                            <span className={styles.title}>Adresse du bien</span>
-                            <span>{engagement?.infos?.bien_adresse}</span>
+                            <div className={styles.title}>Bien</div>
+                            <div>{engagement?.bien?.nom}</div>
                         </div>
 
                         <div className={styles.row}>
-                            <span className={styles.title}>Email</span>
-                            <span>{engagement?.infos?.proprietaire_email}</span>
+                            <div className={styles.title}>Année de construction</div>
+                            <div>{engagement?.buildingYear?.nom}</div>
                         </div>
 
                         <div className={styles.row}>
-                            <span className={styles.title}>Téléphone</span>
-                            <span>{engagement?.infos?.proprietaire_telephone}</span>
+                            <div className={styles.title}>Superficie du bien</div>
+                            <div>{engagement?.surface?.nom}</div>
+                        </div>
+
+                        <div className={styles.row}>
+                            <div className={styles.title}>Annexes</div>
+                            <div>{engagement?.annexes === 0 ? 'Non' : engagement?.annexes}</div>
+                        </div>
+
+                        <div className={styles.row}>
+                            <div className={styles.title}>Diagnostics immobiliers à réaliser</div>
+                            <div>
+                                {engagement?.diagnostics?.map((diag, index) => (
+                                    <div key={diag?.id}>
+                                        {diag?.name}
+                                        {engagement?.diagnostics && index !== engagement?.diagnostics?.length - 1 && ','}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+
+                        <div className={styles.row}>
+                            <div className={styles.title}>Coordonnées</div>
+
+                            <div className={clsx(styles.infos, styles.row)}>
+                                <div className={styles.row}>
+                                    <div className={styles.title}>Nom et prénom</div>
+                                    <div>{engagement?.infos?.proprietaire_nom}</div>
+                                </div>
+
+                                <div className={styles.row}>
+                                    <div className={styles.title}>Adresse du bien</div>
+                                    <div>{engagement?.infos?.bien_adresse}</div>
+                                </div>
+
+                                <div className={styles.row}>
+                                    <div className={styles.title}>Email</div>
+                                    <div>{engagement?.infos?.proprietaire_email}</div>
+                                </div>
+
+                                <div className={styles.row}>
+                                    <div className={styles.title}>Téléphone</div>
+                                    <div>{engagement?.infos?.proprietaire_telephone}</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <br />
+
+                <Button type='primary' style={{ width: 'fit-content' }} onClick={onSelection}>Fixer un rendez-vous</Button>
+            </StepCard>
+            <br></br>
+            <div className={styles.share}> 
+                <Share htmlToShare={htmlToShare}></Share>
             </div>
-            <br />
-            <Button type='primary' style={{ width: 'fit-content' }} onClick={onSelection}>Fixer un rendez-vous</Button>
-        </StepCard>
+        </div>
     );
 }
 
---
+++ src/components/Whatsapp/Share.tsx
@@ -0,0 +1,56 @@
+import { convert } from 'html-to-text';
+import whatsappShare from '../../assets/images/whatsapp.png';
+import smsShare from '../../assets/images/sms.png';
+import { Link } from 'react-router-dom';
+import styles from './Share.module.less';
+
+enum share {
+    WHATSAPP = 'whatsapp',
+    SMS = 'sms'
+}
+
+interface IShareProps {
+    htmlToShare: string | undefined
+}
+
+const Share = ({ htmlToShare }: IShareProps) => {
+
+    const convertHtmlToPlainText = (html: any) => {
+        console.log('text11', html)
+        const plainText = convert(html, { preserveNewlines: true });
+        return plainText;
+    }
+
+    const getWhatsAppShareLink = (text: any) => {
+        console.log('text', text)
+        let encodedText = "Votre Devis: %0A";
+        encodedText += encodeURIComponent(text);
+        encodedText += " %0A N'hésitez pas à nous contacter pour fixer un rendez-vous %0A";
+        encodedText += "Au: 07 55 53 23 33 %0A";
+        encodedText += "secretariat@idzconsulting.fr %0A";
+        encodedText += "IDZ consulting %0A";
+        const url = `https://api.whatsapp.com/send?text=${encodedText}`;
+        return url;
+    }
+
+    function getSmsShareLink(text: any) {
+        const encodedMessage = encodeURIComponent(text);
+        const url = `sms:?body=${encodedMessage}`;
+        return url
+      }
+
+    const handleShareClick = (shareOption: share) => {
+        const plainText = convertHtmlToPlainText(htmlToShare);
+        const shareLink = shareOption === share.WHATSAPP ? getWhatsAppShareLink(plainText) : getSmsShareLink(plainText) 
+        window.open(shareLink);
+    };
+
+    return (
+        <div>
+            <Link onClick={() => handleShareClick(share.WHATSAPP)} to={''}><img className={styles.whatsapp} src={whatsappShare} alt="whatsapp" /> </Link>
+            <Link onClick={() => handleShareClick(share.SMS)} to={''}><img className={styles.whatsapp} src={smsShare} alt="sms" /> </Link>
+        </div>
+    );
+}
+
+export default Share;
